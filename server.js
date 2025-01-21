765ed31f362147bf7f5a5d3c8c95b1721f245382
Adedeji234501 <adedejiazeez1k@gmail.com>
2025-01-21 06:59:54 +0000

    second commit wk1

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./database/database');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-app.listen(port,() => {
-    console.log(`Connected to Database and listening on ${port}`);
+app.use(bodyParser.json());
+  
+app.use('/',require('./routes/contacts')); // Use the routes
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        app.listen(port,() => {
+          console.log(`Connected to Database and listening on ${port}`);
+        });
+    }
 });
